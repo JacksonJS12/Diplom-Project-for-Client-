3fa94b09fb695f27d3695dee1a72a070e2a87d53
Deniz <d.ivan0v.6100@gmail.com>
2023-05-06 00:00:05 +0300

    database update

---
+++ Selim/BazeIconom/Iconom/Iconom/InputOutputFormulaire.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Iconom
+{
+    public partial class InputOutputFormulaire : Form
+    {
+        public InputOutputFormulaire()
+        {
+            InitializeComponent();
+        }
+
+        private void btnReturn_Click(object sender, EventArgs e)
+        {
+            //this.Hide();
+            ReferenceLocataires log = new ReferenceLocataires();
+            log.Show();
+            this.Close();
+        }
+    }
+}
