4da1e8d3e509ae6aa198613d3341c6ffa3c3d9cf
Deniz <d.ivan0v.6100@gmail.com>
2023-05-13 15:58:17 +0300

    connection string updated to be more abstract

--- Selim/BazeIconom/Iconom/Iconom/ConnectionString.cs
+++ Selim/BazeIconom/Iconom/Iconom/ConnectionString.cs
@@ -5,6 +5,7 @@ namespace Iconom
     public class ConnectionString
     {
         //change the DB connection before use
-        public const string DbConnection = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\ricky\source\repos\Diplom-Projects\Selim\BazeIconom\Iconom\Iconom\database.mdf;Integrated Security=True;Connect Timeout=30";
+        public const string DbConnection =
+            @"Data Source=(localdb)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\database.mdf;Integrated Security=True;Connect Timeout=30";
     }
 }
